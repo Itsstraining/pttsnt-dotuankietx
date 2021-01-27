704cfc7752e6037b4656a8359b3561e198bb6d70
dotuankietx <dotuankiet2604@gmail.com>
2021-01-27 23:13:16 +0700

    lan 1

--- program.js
+++ program.js
@@ -1,8 +1,28 @@
 // Add new functions, variables here
 
-function main(input) {
-  // Your code goes here
-  // Using console.log to answer the question
-}
+function snt(input, i, arr) {
+  while (input != 1) {
+    if (input % i == 0) {
+      arr= arr+i+" ";
+      input = input / i
+    } else {
+      i++
+    }
+  }
+  return console.log(arr);
 
+}
+function main(input) {
+
+  let arr = "";
+  let i = 2;
+  if (input == 1) {
+    console.log("1")
+  } else if (input == 0) {
+    console.log("0")
+  } else {
+    snt(input, i, arr);
+    
+  }
+}
 module.exports = main;
